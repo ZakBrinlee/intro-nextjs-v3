53304a670e876969820f905bead6d947ab3a889c
Zak Brinlee <zbrinlee@hotmail.com>
2023-11-03 15:17:44 +1000

    init commit - intro and first section

---
+++ app/(admin)/settings/[[...id]]/page.tsx
@@ -0,0 +1,11 @@
+// Proof of pages being SSR by default, able to do anything you can do in node.js env
+// console.log(process.argv)
+
+const SettingsPage = ({params}) => {
+  // Example of catch-all dynamic route segments
+  // Note: no access to query params on the server
+  console.log(params)
+  return <div>Settings Page example for grouping</div>
+}
+
+export default SettingsPage
